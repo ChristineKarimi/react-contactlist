6071d945dc9b41c5c5042fa350d342c9c871ae4b
christinekarimi <karimikim3@gmail.com>
2021-04-26 13:53:27 +0300

    create contact component to make it reusable

--- src/App.js
+++ src/App.js
@@ -3,15 +3,23 @@ import "./styles.css";
 
 const App = () => {
   return (
-    <div className="contact-card">
-    <img src="https://via.placeholder.com/150" alt="profile" />
-    <div className="user-details">
-      <p>Name: Jenny Han</p>
-      <p>Email: Jenny.Han@notreal.com</p>
-      <p>Age: 25</p>
-    </div>
-  </div>
+    <>
+    <ContactCard />
+  </>
   );
 }
+
+const ContactCard = () => {
+  return (
+    <div className="contact-card">
+      <img src="https://via.placeholder.com/150" alt="profile" />
+      <div className="user-details">
+        <p>Name: Jenny Han</p>
+        <p>Email: Jenny.Han@notreal.com</p>
+        <p>Age: 25</p>
+      </div>
+    </div>
+  );
+};
 
 export default App;
