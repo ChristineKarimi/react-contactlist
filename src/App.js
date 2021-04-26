0243f131884f5d83dee281189e9696b72fc02ed4
christinekarimi <karimikim3@gmail.com>
2021-04-26 14:00:16 +0300

    Add state to store whether user age should be shown or not

--- src/App.js
+++ src/App.js
@@ -5,17 +5,22 @@ const App = () => {
   return (
     <>
     <ContactCard />
+    <ContactCard />
+    <ContactCard />
+
   </>
   );
 }
 
 const ContactCard = () => {
+  const [showAge, setShowAge] = useState(false);
   return (
     <div className="contact-card">
       <img src="https://via.placeholder.com/150" alt="profile" />
       <div className="user-details">
         <p>Name: Jenny Han</p>
         <p>Email: Jenny.Han@notreal.com</p>
+        {showAge === true ? <p>Age: 25</p> : null}
         <p>Age: 25</p>
       </div>
     </div>
